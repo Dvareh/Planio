d16063df285e2018ff26e290bbc3031e592278a3
Dvareh <cobalt574@gmail.com>
2026-04-04 23:15:56 +0200

    task(): Spring boot project setup

---
+++ backend/src/main/java/com/planio/app/PlanioApplication.java
@@ -0,0 +1,13 @@
+package com.planio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanioApplication.class, args);
+	}
+
+}
