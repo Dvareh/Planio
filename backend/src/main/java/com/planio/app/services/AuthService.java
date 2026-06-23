95c5eaacbe3814574f5ec3b734bd5a839125732b
Dvareh <cobalt574@gmail.com>
2026-06-23 17:18:55 +0200

    feat(auth): implemented JWT authentication and Spring Security

---
+++ backend/src/main/java/com/planio/app/services/AuthService.java
@@ -0,0 +1,51 @@
+package com.planio.app.services;
+
+import com.planio.app.dto.AuthDTO;
+import com.planio.app.dto.LoginDTO;
+import com.planio.app.dto.RegisterDTO;
+import com.planio.app.entity.User;
+import com.planio.app.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthDTO register(RegisterDTO registerDTO) {
+
+        User user = User.builder()
+                .username(registerDTO.getUsername())
+                .email(registerDTO.getEmail())
+                .password(passwordEncoder.encode(registerDTO.getPassword()))
+                .build();
+
+        userRepository.save(user);
+
+        String token = jwtService.generateToken(user.getEmail());
+
+        return new AuthDTO(token);
+    }
+
+    public AuthDTO login(LoginDTO loginDTO) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDTO.getEmail(),
+                        loginDTO.getPassword()
+                )
+        );
+
+        String token = jwtService.generateToken(loginDTO.getEmail());
+
+        return new AuthDTO(token);
+    }
+}
